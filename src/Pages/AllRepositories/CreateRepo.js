526d7cb7bd6055771fc74d1b4bd7fed7f4dab179
2213403830 <2213403830@qq.com>
2022-04-18 19:28:07 +0800

    modified:   src/Data/demo.js 	modified:   src/Pages/AllRepositories/CreateRepo.js 	modified:   src/Pages/AllRepositories/RepoOverView.js

--- src/Pages/AllRepositories/CreateRepo.js
+++ src/Pages/AllRepositories/CreateRepo.js
@@ -1,4 +1,3 @@
-import { isVisible } from '@testing-library/user-event/dist/utils';
 import React, { useState } from 'react';
 
 function CreateRepo() {
--- src/Pages/AllRepositories/RepoOverView.js
+++ src/Pages/AllRepositories/RepoOverView.js
@@ -3,7 +3,13 @@ import './RepoOverView.css';
 import { Link, } from "react-router-dom";
 import { getRepositoryInfo } from '../../Data/demo.js'
 import { useEffect, useState } from 'react';
+import { Button } from 'bootstrap';
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 
+async function DeteleRepo(id) {
+    await sleep(Math.round(Math.random() * 2000));
+    return Math.random() < 0.5 ? true : false;
+}
 
 function RepoOverView(props) {
     const [repoInfo, setRepoInfo] = useState(null);
@@ -13,9 +19,9 @@ function RepoOverView(props) {
             console.log(data);
         });
     }, []);
-    return repoInfo?(
+    return repoInfo ? (
         <React.Fragment>
-            <Link to={'/repositoryInfo/'+props.repoName}>
+            <Link to={'/repositoryInfo/' + props.repoName}>
                 <h1 className="repositoryPaperOverView-title">{repoInfo.paperTitle}</h1>
             </Link>
             <div className="repositoryOverView">
@@ -26,8 +32,16 @@ function RepoOverView(props) {
                     <p className="repositoryDataOverView-dataset">{repoInfo.dataSetLink[0]}</p>
                 </div>
             </div>
+            <button onClick={async function () {
+                const res = await DeteleRepo(repoInfo.id)
+                if (res) {
+                    setRepoInfo(null);
+                    alert('成功删除仓库')
+                }
+                else alert('仓库不存在')
+            }}>Delete</button>
         </React.Fragment>
-    ):(
+    ) : (
         <></>
     );
 }
