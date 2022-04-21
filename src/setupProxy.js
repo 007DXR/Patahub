fd42843feeb655b1c8c45549eb5a4da6019a4a11
xzz-233 <xuzhouzi@qq.com>
2022-04-21 16:54:25 +0800

    fixed /api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,15 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function (app) {
+    const rewriteFn = function (path, req) {
+        return path.replace('/api', '/');
+    };
+  
     app.use(
         '/api/',
         createProxyMiddleware({
             target : 'http://sycstudio.com:20729/',
-            changeOrigin : true,  // 设置跨域请求
-            PathRewrite : {
-                '^/api' : '' // 将/api/v1 变为 ''
-            }
+            changeOrigin : true,
+            pathRewrite : rewriteFn 
         })
     );
 };
