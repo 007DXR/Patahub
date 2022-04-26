2bcc11e8c473aca5c2bf64502c25a4dfbf7eb533
keaneu42 <59404252+keaneu42@users.noreply.github.com>
2022-04-26 18:22:50 +0800

    modify rcd overview & add api

--- src/Data/demo.js
+++ src/Data/demo.js
@@ -27,14 +27,18 @@ export async function getRCDList(repoName) {
     {
         resultId:1,
         resultImage: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png',
-        codeLinks: [`https://github.com/007DXR/Patahub`],
-        datasetLinks: [`https://github.com/facebook/react`],
-    },{
+        codeLinks: `https://github.com/007DXR/Patahub`,
+        datasetLinks: `https://github.com/facebook/react`,
+    },
+    {
         resultId:2,
         resultImage: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
-        codeLinks: [`https://github.com/typescript-cheatsheets/react`,`https://github.com/discountry/react`],
-        datasetLinks: [`https://github.com/vnotex/vnote`,`https://www.github.com/${repoName}/xxyfdsafydataset`,`https://www.github.com/${repoName}/xxwwwwydataset`,],
-    },];
+        codeLinks: `https://github.com/typescript-cheatsheets/react`,
+        // ,`https://github.com/discountry/react`
+        datasetLinks: `https://github.com/vnotex/vnote`,
+        // ,`https://www.github.com/${repoName}/xxyfdsafydataset`,`https://www.github.com/${repoName}/xxwwwwydataset`,
+    },
+];
 }
 
 export async function getRCD(repoName, Id) {
--- src/Pages/RepositoryInfo/RepositoryInfo.js
+++ src/Pages/RepositoryInfo/RepositoryInfo.js
@@ -3,39 +3,71 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import RCDOverView from './RCDOverView.js'
 import { getRCDList } from '../../Data/demo.js'
-import { Container, Row } from 'react-bootstrap';
-import EmptyRCDOverView from './EmptyRCDOverView.js';
+import { Card, Container, Row, Button } from 'react-bootstrap';
+// import EmptyRCDOverView from './EmptyRCDOverView.js';
+import { CreateRCD, DelRCD, getRCDByRepoID, getRCDByRepoName, getResultLink } from '../../Data/rcd.js';
+import PostRCDForm from './PostRCD.js';
 
 function RepositoryInfo(props){
-    const repoName = useParams().repoName;
+    const repoId = useParams().repoName;
     const [RCDList, setRCDList] = useState([]);
-    
-    useEffect( () => {
-        getRCDList(repoName).then((data, err) => {
-            setRCDList(data);
-        });
-    } ,[]);
-    
-    function getRCDItem(newRCD){
-        setRCDList([...RCDList, {
-            resultId: RCDList.length+1, 
-            resultImage: newRCD.resultImage,
-            codeLinks: newRCD.codeLinks,
-            datasetLinks: newRCD.datasetLinks,
-        }])
-    }
+    const [isCreating, setIsCreating] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [editingRCD, setEditingRCD] = useState({});
 
-    const delRCD = index => {
-        setRCDList([...RCDList.slice(0, index), ...RCDList.slice(index + 1)]);
+    useEffect( () => {
+        console.log("paper id", repoId, typeof(repoId), typeof(parseInt(repoId)))
+        getRCDByRepoID(parseInt(repoId)).then((data, err) => {
+            console.log("data",data, typeof(data));
+            data = data.map((RCD)=>{
+                const res = getResultLink(RCD.result_id);
+
+                return {
+                    paperId: RCD.paper_id,
+                    resultId: RCD.result_id,
+                    resultImage: res,
+                    codesetId: RCD.codeset_id,
+                    datasetId: RCD.dataset_id,
+                    codeLink: RCD.code_link,
+                    dataLink: RCD.data_link,
+                    rcdId: RCD.rcd_id
+                };
+            })
+            setRCDList(data);
+            setIsDeleting(false);
+            console.log("data2",data, typeof(data));
+        });
+    } ,[isCreating, isEditing, isDeleting]);
+    //     getRCDList(repoId).then((data, err) => {
+    //         console.log("data", data)
+    //         setRCDList(data);
+    //     });
+    // } ,[]);
+
+    const delRCD = rcdID => {
+        const res = DelRCD(rcdID);
+        setIsDeleting(true);
+    }
+    const hideForm = () => {
+        if(isCreating){
+            setIsCreating(false);
+        }
+        if(isEditing){
+            setIsEditing(false);
+            setEditingRCD({});
+        }
     }
 
     return (
         <Container className='pt-5'>
             {
-                RCDList.map((RCD, index) => <RCDOverView repoName={repoName} RCD={RCD}
-                onRemove={() => delRCD(index)}/>)
+                RCDList.map((RCD) => <Card className="m-3 p-3"><RCDOverView repoName={repoId} RCD={RCD}
+                onRemove={(rcdID) => delRCD(rcdID)} onEdit={(RCD)=>{setIsEditing(true);setEditingRCD(RCD)}}/></Card>)
             }
-            <Row><EmptyRCDOverView sendValueToFa={getRCDItem.bind(this)}/></Row>
+            {/* <Row><EmptyRCDOverView sendValueToFa={getRCDItem.bind(this)}/></Row> */}
+            <Button onClick={() => setIsCreating(true)}>add</Button>
+            <PostRCDForm onCreate={isCreating} onEdit={isEditing} RCD={editingRCD} onHide={hideForm} fixedPaperID={repoId}></PostRCDForm>
         </Container>
     )
 }
